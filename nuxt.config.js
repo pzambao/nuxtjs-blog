2e0cc878c2251d49a977dcf80309d8dd2a00b5d3
pzambao <pedrozambaoo@gmail.com>
2024-11-18 02:36:28 -0300

    feat: vercel adjusts

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  ssr: true,
   target: 'server',
-  buildDir: '.output',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
