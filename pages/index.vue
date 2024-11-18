756c97c9431324390149b4b2c1f620a065190240
pzambao <pedrozambaoo@gmail.com>
2024-11-18 20:29:38 -0300

    feat: header adjust

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="bg-gray-900 h-screen text-white ">
     <Navbar />
-    <Head>
-      <title>Início | NextJS Form</title>
-      <meta name="description" content="Validação de CEP para estudos utilizando NextJS, Typescript e Tailwindcss" />
-    </Head>
     <div class="flex justify-center flex-col items-center h-[75%] px-3">
       <h1 class="mb-4 text-xl text-center">
         <b>Projeto para estudos:</b>
@@ -21,7 +17,19 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'StorePage'
+  name: 'IndexPage',
+  head () {
+    return {
+      title: 'Início | NuxtJS Form',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Validação de CEP para estudos utilizando NuxtJS, Typescript e Tailwindcss'
+        }
+      ]
+    }
+  }
 })
 </script>
 
